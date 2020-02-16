3cdb67eb8d35d02c2554a62c9c87e8dac06f9ccf
Neos21 <neos21@gmail.com>
2020-02-16 15:24:11 +0900

    Modify

--- exec_markovify.py
+++ exec_markovify.py
@@ -1,49 +1,61 @@
 import os
+import random
 import sys
 
 import markovify
 
+# パラメータを生成する
+state_size           = random.randint(1, 5)          # state_size : 1〜5 の間くらいがまともな文章になる
+is_use_make_sentence = random.choice((True, False))  # make_sentence() を使うか make_short_sentence() を使うか
+max_chars            = random.randint(20, 280)       # make_short_sentence() の場合の最大文字数 : 20 以上で日本語10文字程度の文章が成り立つようになる
+
+print('State Size : ', state_size)
+print('Method     : ', 'make_sentence' if is_use_make_sentence else 'make_short_sentence')
+print('Max Chars  : ', max_chars if not is_use_make_sentence else '(Not Use)')
+print('')
+
 # モデルを生成する
-model = None
-#if os.path.exists('./learned.json'):
-#  # 既に学習済モデルがあればそれを利用する
-#  #print('Use Learned JSON Data')
-#  with open('./learned.json', 'r') as file:
-#    model = markovify.NewlineText.from_json(file.read())
-#else:
-  # 学習済モデルがなければテキストファイルからモデルを生成する
-  #print('Use Text File')
-text = open('./splitted.txt', 'r').read()
-model = markovify.NewlineText(text, state_size = 3)
+text = open('./splitted.txt', 'r', encoding = 'utf-8').read()
+model = markovify.NewlineText(text, state_size = state_size)
 
-"""
-# 文章を生成する
-sentence = model.make_sentence(tries = 1000)
-
-# 文章生成に失敗したら None が返る
-if sentence is None:
-  print('上手く生成できませんでした')
-  sys.exit()
-
-# 分かち書きされているのを結合して出力する
-#print('----------')
-print(''.join(sentence.split()))
-#print('----------')
-
-if not os.path.exists('./learned.json'):
-  #print('Write Learned JSON Data')
-  with open('./learned.json', 'w') as file:
-    file.write(model.to_json())
-"""
+# リトライ回数の管理用
+retry_count = 0
+max_retry_count = 5
 
 # 異なる文章を10個列挙させる
 sentences = []
 while len(sentences) < 10:
-  sentence = model.make_sentence(tries = 1000)
-  if sentence is not None:
-    new_sentence = ''.join(sentence.split())
-    if new_sentence not in sentences:
-      print(new_sentence)
-      sentences.append(new_sentence)
+  # 文章生成を試みる
+  sentence = None
+  if is_use_make_sentence:
+    sentence = model.make_sentence(tries = 1000)
+  else:
+    sentence = model.make_short_sentence(tries = 1000, max_chars = 40)
+  
+  # 文章が生成できなかったらリトライする
+  if sentence is None:
+    retry_count += 1
+    if retry_count <= max_retry_count:
+      continue
+    else:
+      break
+  
+  # 文章の分かち書きのスペースを除去する
+  new_sentence = ''.join(sentence.split())
+  
+  # 既出の文章の場合はリトライする
+  if new_sentence in sentences:
+    retry_count += 1
+    if retry_count <= max_retry_count:
+      continue
+    else:
+      break
+  
+  # 初出の文章を出力する
+  print(new_sentence)
+  sentences.append(new_sentence)
+
+print('')
+print('Created : ', len(sentences))
 
 print('\nEnd')
--- split_for_markovify.py
+++ split_for_markovify.py
@@ -4,40 +4,38 @@ import MeCab
 breaking_chars = ['(', ')', '[', ']', '"', "'"]
 
 # 基となるテキストをファイルから読み込む
-text = open('./input.txt', 'r').read()
+text = open('./input.txt', 'r', encoding = 'utf-8').read()
 
 #mecab = MeCab.Tagger()
 mecab = MeCab.Tagger('-d /usr/local/lib/mecab/dic/mecab-ipadic-neologd')
 
-# 1行ごとに処理する
+# テキストを一文ごとに改行し、一文の語句をスペースで分割する
 splitted_text = ''
-for line in text.split():
-  # テキストを一文ごとに改行し、一文の語句をスペースで分割する
+for line in text.splitlines():
   #print('Line : ', line)
   parsed_nodes = mecab.parseToNode(line)
   while parsed_nodes:
-    print('Surface : ', parsed_nodes.surface)
+    #print('Surface : ', parsed_nodes.surface, ' : ', len(parsed_nodes.surface))
     try:
-      # 上手く解釈できない文字列は飛ばして結合する
-      if parsed_nodes.surface not in breaking_chars:
-        #print('  OK')
+      if len(parsed_nodes.surface) == 0:  # 空文字は飛ばす
+        continue
+      if parsed_nodes.surface not in breaking_chars:  # うまく結合できない文字のみ除外し結合する
         splitted_text += parsed_nodes.surface
-      # 句読点でなければスペースで分かち書きする
-      if parsed_nodes.surface != '。' and parsed_nodes.surface != '、':
-        #print('  スペース付与')
+      if parsed_nodes.surface != '。' and parsed_nodes.surface != '、':  # 句読点でなければスペースで分かち書きする
         splitted_text += ' '
-      # 句点が登場したら改行で文章を分割する
-      if parsed_nodes.surface == '。':
-        #print('  改行付与')
+      if parsed_nodes.surface == '。':  # 句点が登場したら改行する
         splitted_text += '\n'
     except UnicodeDecodeError as error:
       print('Error : ', line)
     finally:
       parsed_nodes = parsed_nodes.next
+  # 1行終了
+  splitted_text += '\n'
 
-#print('Result :\n', splitted_text)
+# 空白行を消す
+splitted_text = '\n'.join(filter(lambda x: x.strip(), splitted_text.split('\n')))
 
-with open('./splitted.txt', 'w') as file:
+with open('./splitted.txt', 'w', encoding = 'utf-8') as file:
   file.write(splitted_text)
 
-print('End')
+print('\nEnd')
